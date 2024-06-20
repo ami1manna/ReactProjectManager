cd3c784e7f503e6c6557cafe48734e7616599975
ami1manna <amitmannasm@gmail.com>
2024-06-20 13:52:36 +0530

    added new project , form to add new project and Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
 import NewProject from "./component/NewProject";
 import NoProjectSelected from "./component/NoProjectSelected";
 import ProjectsSideBar from "./component/ProjectsSideBar";
-
+import { useState } from "react";
 function App() {
+    // undefined no project and not selected
+    // null have project but not selected
+    // id - selected project
+    const [projectsState,setProjectsState] =  useState({
+      selectedProjectId:undefined,
+      projects:[]
+  });
+function handleStartAddProject(){
+  setProjectsState(prevState=>{
+      return {
+          ...prevState,
+          selectedProjectId:null,
+      };
+  });
+}
+
+function handleAddProject(projectData){
+
+  setProjectsState(prevState=>{
+  const newProject = {
+    ...projectData,
+    id:Math.random()
+  };
+    return {
+      ...prevState,
+      selectedProjectId:undefined,
+      projects:[...prevState.projects, newProject]
+    }
+  })
+}
+
+let content;
+if(projectsState.selectedProjectId===null){
+  content = <NewProject onAdd={handleAddProject}/>
+}
+else if(projectsState.selectedProjectId === undefined){
+content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+}
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSideBar/>
-      <NoProjectSelected/>
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
 
     </main>
   );
--- src/component/NewProject.jsx
+++ src/component/NewProject.jsx
@@ -1,19 +1,60 @@
 import React from 'react'
-import  Input  from '../component/Input';
-const NewProject = () => {
+import Input from '../component/Input';
+import Modal from '../component/Modal';
+import { useRef } from 'react';
+const NewProject = ({onAdd}) => {
+  const title = useRef()
+  const description = useRef()
+  const dueDate = useRef()
+  const modal = useRef()
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value; 
+
+    
+    //validation ...
+    if(enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim()==='') {
+      // show error modal
+      modal.current.open();
+      
+      return
+    }
+    onAdd({
+      title:enteredTitle,
+      description:enteredDescription,
+      dueDate:enteredDueDate
+    })
+
+  }
+
+ 
   return (
+    <>
+    <Modal ref={modal} buttonCaption='Okay'>
+      <h2 className='text-xl  font-bold text-stone-700 my-4' >Invalid Input</h2>
+      <p className='text-stone-600 mb-4' >Oops ... looks like you forgot to enter a value</p>
+      <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field</p>
+    </Modal>
     <div className='w-[35rem] mt-16'>
       <menu className='flex items-center justify-end gap-4 my-4'>
         <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
-        <li><button className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save</button></li>
+        <li>
+          <button 
+          className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
+          onClick={handleSave}
+          >Save</button>
+          </li>
       </menu>
       <div>
-       <Input label="Title"/>
-       <Input label="Description" textarea/>
-       <Input label="Due Date"/>
-       
+        <Input label="Title" ref={title} type="text" />
+        <Input label="Description" textarea ref={description} />
+        <Input label="Due Date" ref={dueDate} type="date" />
+
       </div>
     </div>
+    </>
   )
 }
 
